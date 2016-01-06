06c7a185819f96803b69d7341adae5c9ed810210
treestompz <sforstefan@hotmail.com>
2016-01-05 20:38:37 -0500

    modified to use given text files with cp1252

--- demo.py
+++ demo.py
@@ -1,9 +1,25 @@
-from pyteaser import SummarizeUrl
-from pprint import pprint
-urls = (u'http://www.huffingtonpost.com/2013/11/22/twitter-forward-secrecy_n_4326599.html',
-        u'http://www.bbc.co.uk/news/world-europe-30035666',
-        u'http://www.bbc.co.uk/news/magazine-29631332')
+from pyteaser import Summarize
 
-for url in urls:
-    summaries = SummarizeUrl(url)
-    pprint(summaries)
+def summarize_file(filename):
+    with open(filename, 'r') as f:
+        title = f.readline().decode('cp1252')
+
+        content = ""
+        for line in f:
+            if line:
+                content += line.decode('cp1252')
+
+        print filename + " / " + title
+        summary = Summarize(title, content)
+
+        for bullet_point in summary:
+            print "[+] " + bullet_point
+
+
+summarize_file("500words.txt")
+print ""
+print ""
+summarize_file("1000words.txt")
+print ""
+print ""
+summarize_file("2000words.txt")
